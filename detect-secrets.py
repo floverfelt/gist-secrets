5990d1aa64deaa51381d11287e59af8d9e001606
Overfelt, Jacob <florian.overfelt@optum.com>
2021-09-24 14:26:48 -0400

    Add timeout to detect-secrets.

--- detect-secrets.py
+++ detect-secrets.py
@@ -1,7 +1,17 @@
 from detect_secrets import SecretsCollection
 from detect_secrets.settings import default_settings
+import timeout_decorator
 import json
 import sys
+
+
+@timeout_decorator.timeout(10)
+def detect_secrets(file):
+    secrets = SecretsCollection()
+    with default_settings():
+        secrets.scan_file(file)
+
+    return secrets
 
 
 if __name__ == "__main__":
@@ -14,10 +24,7 @@ if __name__ == "__main__":
 
         # 0th arg is the file name
         file = sys.argv[1]
-        secrets = SecretsCollection()
-        with default_settings():
-            secrets.scan_file(file)
-
+        secrets = detect_secrets(file)
         print(json.dumps(secrets.json(), indent=2))
 
     except:
